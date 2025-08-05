0195b0aae842012d10be47bc8ce6efe46610e066
Ncorbin0809 <ncorbin717@gmail.com>
2025-08-04 22:41:51 -0500

    feat: final submission for froyo assignment

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-const flavorPrompt = () => {
+//prompt for list of flavors//
+function flavorPrompt() {
   const userInput = prompt('Please enter your favorite froyo flavors!')
-
-  return userInput.split(',')
+  //return user entry in comma seperated strings//
+  return userInput.split(',').map((element) => element.trim())
 }
+//iterate through and count num of flavors and save to object
+function countNumOfFlavors(flavorArray) {
+  const flavorCountTracker = {}
+
+  for (const flavor of flavorArray) {
+    flavorCountTracker[flavor] = (flavorCountTracker[flavor] || 0) + 1
+  }
+  return flavorCountTracker
+}
+
+//start the functions getting user input, counting flavors and logging them//
+const start = flavorPrompt()
+const countOfFlavors = countNumOfFlavors(start)
+
+console.log('Flavor count:')
+console.log(countOfFlavors)
